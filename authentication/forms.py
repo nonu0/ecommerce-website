8f881c9ae8b51ac7c6d4b827d7257ceea22f9ee2
cliff <cliffowino22@gmail.com>
2023-03-18 23:52:21 +0300

    forms.py

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from website.models import *
-
+from django.contrib.auth import logout,login,authenticate,get_user_model
+User = get_user_model()
 class RegisterForm(forms.ModelForm):
     password = forms.PasswordInput()
     confirm_password = forms.PasswordInput()
@@ -9,4 +10,5 @@ class RegisterForm(forms.ModelForm):
         fields = ['fname','lname','email','username','address']
     def clean_username(self):
         username = self.cleaned_data.get('username')
-        if User.objects.filter()
+        if User.objects.filter(username=username):
+            raise ValueError('username already taken')
