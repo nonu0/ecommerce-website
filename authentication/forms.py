02a7fe237bd00c9bd15499ba3ae09f0daad2e036
cliff <cliffowino22@gmail.com>
2023-03-19 11:20:56 +0300

    register views

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from website.models import *
-from django.contrib.auth import logout,login,authenticate,get_user_model
+from django.contrib.auth import get_user_model
 User = get_user_model()
 class RegisterForm(forms.ModelForm):
     password = forms.PasswordInput()
     confirm_password = forms.PasswordInput()
     class Meta:
         model = Customer
-        fields = ['fname','lname','email','username','address']
+        fields = ['fname','lname','email','username','address','password','confirm_password']
     def clean_username(self):
         username = self.cleaned_data.get('username')
-        if User.objects.filter(username=username):
+        if User.objects.filter(username=username).exists():
             raise ValueError('username already taken')
+        return username
--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView,CreateView
+from authentication.forms import RegisterForm
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+User = get_user_model()
 # Create your views here.
 
-class RegisterView(TemplateView):
+class RegisterView(CreateView):
     template_nme = 'login.html'
+    form_class = RegisterForm
+    def form_valid(self, form):
+        uname = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        confirm_password = form.cleaned_data.get('confirm_password')
+        
+        if password == confirm_password:
+            user = User.objects.create_user(email=email,username=uname,password=password)
+            form.instance.user = user
+            instance = form.save(commit=False)
+            instance.customer
+            form.save()
+            return redirect('authentication:login')
+        else:
+            return ValidationError(self.request,'Passwords must match')
+    
+
+
 
 
 class LoginView(TemplateView):
