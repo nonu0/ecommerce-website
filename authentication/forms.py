7dbb8a5347e2bf7dd39e55a0b7e8270572359966
cliff <cliffowino22@gmail.com>
2023-03-19 19:33:07 +0300

    login view

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,8 +3,8 @@ from website.models import *
 from django.contrib.auth import get_user_model
 User = get_user_model()
 class RegisterForm(forms.ModelForm):
-    password = forms.PasswordInput()
-    confirm_password = forms.PasswordInput()
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = Customer
         fields = ['fname','lname','email','username','address']
@@ -13,3 +13,7 @@ class RegisterForm(forms.ModelForm):
         if User.objects.filter(username=username).exists():
             raise ValueError('username already taken')
         return username
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=10)
+    password = forms.CharField(widget=forms.PasswordInput())
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render,redirect
 from django.views.generic import TemplateView,CreateView,FormView
-from authentication.forms import RegisterForm
-from django.contrib.auth import get_user_model
+from authentication.forms import RegisterForm,LoginForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import resolve_url
+from django.conf import settings
+from django.contrib.auth import get_user_model,login,authenticate,logout
 from django.core.exceptions import ValidationError
+from django.contrib import messages
 User = get_user_model()
 # Create your views here.
 
@@ -32,3 +36,37 @@ class RegisterView(FormView):
 
 class LoginView(TemplateView):
     template_nme = 'login.html'
+    form_class = LoginForm
+
+    def dispatch(self, request, *args, **kwargs):
+        
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            redirect_to = self.get_success_url()
+            if redirect_to == self.request.path:
+                raise ValueError(
+                    "Redirection loop for authenticated user detected. Check that "
+                    "your LOGIN_REDIRECT_URL doesn't point to a login page."
+                )
+            return HttpResponseRedirect(redirect_to)
+        return super().dispatch(request, *args, **kwargs)
+    
+    def form_valid(self,form):
+        uname = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(self.request,username=uname,password=password)
+        if not user.is_verified:
+            messages.add_message(self.request,'Email is not verified')
+            return render(self.request,'authentication:activate_email.html')
+        if user is not None and user.email_verified:
+            login(self.request,user=user)
+        else:
+            return render(self.request,self.template_name,{'form':self.form_class,'error':'invalid credentials'})
+        return super().form_valid(form)
+
+    def get_default_redirect_url(self):
+        """Return the default redirect URL."""
+        if self.next_page:
+            return resolve_url(self.next_page)
+        else:
+            return resolve_url(settings.LOGIN_REDIRECT_URL)
+    
